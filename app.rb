45afef4e7cd13755e7939b1b23d29b0a28bcd9da
bannastre <bannastre@gmail.com>
2017-05-24 17:49:52 +0100

    Enable sessions - implement session params - add /play path

--- app.rb
+++ app.rb
@@ -2,13 +2,19 @@ require "sinatra/base"
 
 class Battle < Sinatra::Base
 
+enable :sessions
+
 get '/' do
   erb :index
 end
 
 post '/names' do
-  @player1 = params[:player1]
-  @player2 = params[:player2]
+  session['player1'] = params[:player1]
+  session['player2'] = params[:player2]
+  redirect to('/play')
+end
+
+get '/play' do
   erb :play
 end
 
