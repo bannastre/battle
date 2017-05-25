15a48f172b4e5db2cc9e52c03955ec829a624611
Mário Ribeiro <mail@marioribeiro.com>
2017-05-25 10:02:52 +0100

    Add web helpers

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -2,18 +2,12 @@ require './app.rb'
 
 feature 'Start a fight' do
   scenario 'Can enter name and submit' do
-    visit '/'
-    fill_in "player1", :with => "Sally"
-    fill_in "player2", :with => "Geoff"
-    click_button "Fight!"
+    sign_in_and_play
     expect(page).to have_content("Sally vs Geoff")
   end
 
   scenario 'Player 1 can see Player 2 hit points' do
-    visit '/'
-    fill_in "player1", :with => "Sally"
-    fill_in "player2", :with => "Geoff"
-    click_button "Fight!"
+    sign_in_and_play
     expect(page).to have_content("60/60 HP")
   end
 end
