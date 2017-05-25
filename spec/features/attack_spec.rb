9e04306236e1d300b0c5d6dda874d9b688921a70
Mário Ribeiro <mail@marioribeiro.com>
2017-05-25 14:12:31 +0100

    Refactor tests naming

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,9 +1,16 @@
 require './app.rb'
 
 feature 'Attacking' do
+  
   scenario 'Player 1 can attack Player 2 and get confirmation' do
     sign_in_and_play
     click_link('Attack!')
     expect(page).to have_content('Sally attacked Geoff')
   end
+
+  scenario 'Player 1 can see Player 2 hit points' do
+    sign_in_and_play
+    expect(page).to have_content("60/60 HP")
+  end
+
 end
