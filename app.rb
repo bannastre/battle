d77dba1bf27447ffbad55bbd9e0f47a65d98082b
Mário Ribeiro <mail@marioribeiro.com>
2017-05-25 14:09:45 +0100

    Add Player class... and remind us forever to not EVER USE GLOBAL VARIABLES.
    The purpose of this exercise is to remind us of this!!!!

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
-require "sinatra/base"
+require 'sinatra/base'
+require 'player'
 
 class Battle < Sinatra::Base
 
@@ -9,18 +10,22 @@ get '/' do
 end
 
 post '/names' do
-  session['player1'] = params[:player1]
-  session['player2'] = params[:player2]
+  $player1 = Player.new(params[:player1])
+  $player2 = Player.new(params[:player2])
   redirect to('/play')
 end
 
 get '/play' do
+  @player1 = $player1.name
+  @player2 = $player2.name
   erb :play
 end
 
 get '/attack' do
+  @player1 = $player1.name
+  @player2 = $player2.name
   erb :attack
 end
 
-  run! if app_file ==$0
+run! if app_file ==$0
 end
