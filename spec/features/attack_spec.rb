7eea37f61182e3f252233828aadc48b8ea4f59c5
Christopher Harrop <bannastre@gmail.com>
2017-05-25 15:04:02 +0100

    Refactor to make dynamic

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,7 @@
 require './app.rb'
 
 feature 'Attacking' do
-  
+
   scenario 'Player 1 can attack Player 2 and get confirmation' do
     sign_in_and_play
     click_link('Attack!')
@@ -11,6 +11,12 @@ feature 'Attacking' do
   scenario 'Player 1 can see Player 2 hit points' do
     sign_in_and_play
     expect(page).to have_content("60/60 HP")
+  end
+
+  scenario "Player 2's hit_points are reduced when attacked" do
+    sign_in_and_play
+    click_link('Attack!')
+    expect(page).to have_content("Geoff: 50/60 HP")
   end
 
 end
