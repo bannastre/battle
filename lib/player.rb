1fca5afe6370be7f6783881a2179439c91919a26
Christopher Harrop <bannastre@gmail.com>
2017-05-25 14:47:32 +0100

    Players hit points reduce when attacked - implement #take_hit and #reduce_hp(private)

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,25 @@
 
 class Player
 
-  attr_reader :name
+  attr_reader :name, :hit_points
 
   def initialize(name)
     @name = name
+    @hit_points = 60
+  end
+
+  def attack(player, damage = 10)
+    player.take_hit(damage)
+  end
+
+  def take_hit(damage)
+    reduce_hp(damage)
+  end
+
+  private
+
+  def reduce_hp(damage)
+    @hit_points -= damage
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,25 @@
 require 'player'
 
 describe Player do
-  subject(:player) { described_class.new('Sally') }
+  subject(:player1) { described_class.new('Sally') }
+  subject(:player2) { described_class.new('Geoff') }
 
   describe '#name' do
     it 'returns the name' do
-      expect(player.name).to eq 'Sally'
+      expect(player1.name).to eq 'Sally'
+    end
+  end
+
+  describe '#attack' do
+    it 'attacks opponent' do
+      expect(player2).to receive(:take_hit)
+      player1.attack(player2)
+    end
+  end
+
+  describe '#take_hit' do
+    it 'reduces hit points when attacked' do
+      expect { player1.attack(player2, 10) }.to change { player2.hit_points }.by(-10)
     end
   end
 
