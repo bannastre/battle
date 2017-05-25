47a9d04070ce42f20b0c1cbf6928df42075c88e8
Christopher Harrop <bannastre@gmail.com>
2017-05-25 22:48:19 +0100

    refactor controller

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,14 +2,26 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player1, player2) }
   let(:player1) { double(:player1, name: 'Sally') }
   let(:player2) { double(:player2, name: 'Geoff') }
 
+  describe '#player1' do
+    it 'retrieves the first player' do
+      expect(game.player1).to eq game.player1
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player2).to eq game.player2
+    end
+  end
+
   describe '#attack' do
     it 'damages the player' do
-      expect(player2).to receive(:receive_damage)
-      game.attack(player2)
+      expect(game.player2).to receive(:receive_damage)
+      game.attack(game.player2)
     end
   end
 
