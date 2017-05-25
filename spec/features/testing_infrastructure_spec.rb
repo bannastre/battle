8bd5ccedd13712110029ed8e85ed6f9e2eab6389
Mário Ribeiro <mail@marioribeiro.com>
2017-05-25 09:59:34 +0100

    Add Player 2 HP (hit points)

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,4 +8,12 @@ feature 'Start a fight' do
     click_button "Fight!"
     expect(page).to have_content("Sally vs Geoff")
   end
+
+  scenario 'Player 1 can see Player 2 hit points' do
+    visit '/'
+    fill_in "player1", :with => "Sally"
+    fill_in "player2", :with => "Geoff"
+    click_button "Fight!"
+    expect(page).to have_content("60/60 HP")
+  end
 end
